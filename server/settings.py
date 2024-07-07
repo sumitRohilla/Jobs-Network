2b2cb65883214c72db99e7bb8ca46db7676db819
Sumit Rohilla <sumitrohilla171996@gmail.com>
2024-07-08 01:53:02 +0530

    Update settings.py

--- server/settings.py
+++ server/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = env("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["localhost", ".vercel.app", ".now.sh" "127.0.0.1"]
+ALLOWED_HOSTS = ["localhost", ".vercel.app", ".now.sh" "127.0.0.1","api-job-network.onrender.com"]
 
 
 # Application definition
