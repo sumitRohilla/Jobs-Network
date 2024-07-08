188edc0dc0469d8738606389b2d419f0aebe2572
unknown <sumitrohilla171996@gmail.com>
2024-07-09 02:37:42 +0530

    render file

--- server/settings.py
+++ server/settings.py
@@ -7,7 +7,7 @@ env = environ.Env()
 environ.Env.read_env(os.path.join(BASE_DIR, ".env"))
 
 SECRET_KEY = env("SECRET_KEY")
-DEBUG = False
+DEBUG = True
 ALLOWED_HOSTS = ["localhost", "127.0.0.1", "jobs-network.onrender.com"]
 
 INSTALLED_APPS = [
