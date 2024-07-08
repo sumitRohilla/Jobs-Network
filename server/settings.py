d8049e2274986ae7a471c0f82f9d4b4af7ca2138
unknown <sumitrohilla171996@gmail.com>
2024-07-09 02:43:08 +0530

    render file

--- server/settings.py
+++ server/settings.py
@@ -74,8 +74,9 @@ USE_I18N = True
 USE_TZ = True
 
 STATIC_URL = "/static/"
-# STATICFILES_DIRS = [os.path.join(BASE_DIR, 'frontend_build/assets')]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'frontend_build/assets')]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
